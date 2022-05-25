4021c8c9358ec9d86f33ae11fd2880b51f935164
Huang Shewei <huangshewei@gmail.com>
2022-05-25 14:33:55 -0400

    Listing all the comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore'
+import Avatar from '../../components/Avatar'
 
 export default function ProjectComments({ project }) {
   const { updateDocument, response } = useFirestore('projects')
@@ -29,6 +30,24 @@ export default function ProjectComments({ project }) {
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL}/>
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+
 
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
